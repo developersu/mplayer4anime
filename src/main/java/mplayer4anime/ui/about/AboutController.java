2a1e2ba3099195720683f07192dfc5fb59fdfda9
Dmitry Isaenko <developer.su@gmail.com>
2023-05-12 03:41:29 +0300

    Correct UI, update dependencies versions

--- src/main/java/mplayer4anime/ui/about/AboutController.java
+++ src/main/java/mplayer4anime/ui/about/AboutController.java
@@ -4,6 +4,7 @@ import javafx.application.HostServices;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
@@ -23,6 +24,27 @@ public class AboutController implements Initializable {
     private Button buttonOk;
     @FXML
     private TextArea GSONLicense;
+    @FXML
+    private Label versionLbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        versionLbl.setText(ResourceBundle.getBundle("app").getString("_version"));
+
+        GSONLicense.setText("Copyright 2008 Google Inc.\n" +
+                "\n" +
+                "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                "you may not use this file except in compliance with the License.\n" +
+                "You may obtain a copy of the License at\n" +
+                "\n" +
+                "    http://www.apache.org/licenses/LICENSE-2.0\n" +
+                "\n" +
+                "Unless required by applicable law or agreed to in writing, software\n" +
+                "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                "See the License for the specific language governing permissions and\n" +
+                "limitations under the License.");
+    }
 
     @FXML
     private void buttonClickOk(){
@@ -52,24 +74,5 @@ public class AboutController implements Initializable {
         try {
             hostServices.showDocument("https://materialdesignicons.com/");
         } catch (Exception ignored){ }
-    }
-
-    //TODO note icons meaterial design
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        GSONLicense.setText("Copyright 2008 Google Inc.\n" +
-                "\n" +
-                "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                "you may not use this file except in compliance with the License.\n" +
-                "You may obtain a copy of the License at\n" +
-                "\n" +
-                "    http://www.apache.org/licenses/LICENSE-2.0\n" +
-                "\n" +
-                "Unless required by applicable law or agreed to in writing, software\n" +
-                "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                "See the License for the specific language governing permissions and\n" +
-                "limitations under the License.");
     }
 }
